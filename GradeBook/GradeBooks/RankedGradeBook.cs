8d1886f9504b4bb6ddf74af3091b15a6cf6084af
MMarthi <49488904+MMarthi@users.noreply.github.com>
2019-04-10 12:47:08 -0400

    Completed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,15 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using GradeBook.Enums;
+﻿using GradeBook.Enums;
+using System;
+
 
 namespace GradeBook.GradeBooks
 {
     class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count >= 5)
+            {
+                if (averageGrade >= 80)
+                {
+                    return 'A';
+                }
+                else if (averageGrade < 80 && averageGrade > 60)
+                {
+                    return 'B';
+                }
+                else if (averageGrade < 60 && averageGrade > 40)
+                {
+                    return 'C';
+                }
+                else if (averageGrade < 40 && averageGrade > 20)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'F';
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
         }
     }
 }
